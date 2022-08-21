65554544e7b90e06f151a416b728d7a2a91a19fc
peaashmeter <40042317+peaashmeter@users.noreply.github.com>
2022-08-21 21:25:24 +0300

    scrolling

--- lib/songview.dart
+++ lib/songview.dart
@@ -2,18 +2,85 @@ import 'package:flutter/material.dart';
 
 import 'package:html/parser.dart';
 
-class SongView extends StatelessWidget {
+class SongView extends StatefulWidget {
   final String html;
   final String title;
 
   const SongView({super.key, required this.html, required this.title});
+
+  @override
+  State<SongView> createState() => _SongViewState();
+}
+
+class _SongViewState extends State<SongView> {
+  late ScrollController controller;
+  int speed = 0;
+
+  @override
+  void initState() {
+    controller = ScrollController();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text(title),
+          title: Text(widget.title),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                IconButton(
+                  onPressed: () {
+                    setState(() {
+                      speed = 0;
+                      controller.jumpTo(controller.offset);
+                    });
+                  },
+                  icon: Icon(
+                    Icons.pause_rounded,
+                    size: 32,
+                    color: speed != 0 ? Colors.black : Colors.pink,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    setState(() {
+                      speed = 1;
+                      controller.animateTo(controller.position.maxScrollExtent,
+                          duration: Duration(minutes: 3), curve: Curves.linear);
+                    });
+                  },
+                  icon: Icon(
+                    Icons.keyboard_arrow_right_rounded,
+                    size: 32,
+                    color: speed != 1 ? Colors.black : Colors.pink,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    setState(() {
+                      speed = 2;
+                      controller.animateTo(controller.position.maxScrollExtent,
+                          duration: Duration(minutes: 2), curve: Curves.linear);
+                    });
+                  },
+                  icon: Icon(
+                    Icons.keyboard_double_arrow_right_rounded,
+                    color: speed != 2 ? Colors.black : Colors.pink,
+                    size: 32,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
         body: SingleChildScrollView(
+          physics: speed == 0 ? null : NeverScrollableScrollPhysics(),
+          controller: controller,
           child: Row(
             children: [
               Expanded(
@@ -23,7 +90,7 @@ class SongView extends StatelessWidget {
                     fit: BoxFit.scaleDown,
                     child: RichText(
                       text: TextSpan(
-                          text: getSongAsText(html),
+                          text: getSongAsText(widget.html),
                           style: TextStyle(
                               fontSize: 16, color: Colors.blueGrey[900])),
                     ),
